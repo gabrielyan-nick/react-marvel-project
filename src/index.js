418548d661cd9e1b6ef83ff00b5b978309c8a1be
Nicolaj <junk72305@gmail.com>
2023-06-23 17:54:46 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import "../src/styles/style.scss";
 import App from "../src/components/app/App.js";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-  <App />
-   </React.StrictMode>
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
 );
